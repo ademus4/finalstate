5eb3c9945ad0cf88d34984f7a233c21b5b7b8f95
Adam Thornton <ademus4@gmail.com>
2020-04-06 15:25:00 +0100

    Adding other topos to match haspect code

--- Create_Pi2.C
+++ Create_Pi2.C
@@ -1,13 +1,16 @@
 {
   auto FS = adamt::Pi2::Make("ALL","ALL");
   FS->AddTopology("Electron:Proton:Pip:Pim");
+  FS->AddTopology("Electron:Proton:Pim");
+  FS->AddTopology("Electron:Proton:Pip");
+  FS->AddTopology("Electron:Pip:Pim");
 
   //Save TreeDataPi2
   FS->UseOutputRootTree();
 
   /////Make particle trees first in case want to add cut flags
   ParticleDataManager pdm{"particle",1};
-  CLAS12ParticleOutEvent0 pout;
+  CLAS12ParticleOutEvent pout;
   pdm.SetParticleOut(pout);
   FS->RegisterPostTopoAction(pdm);
 
